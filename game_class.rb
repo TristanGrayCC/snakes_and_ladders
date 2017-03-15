2cd3b18de72c5b5fbcc676974cd753691b19f6d9
Tristan Gray <tristangraycc@gmail.com>
2017-03-15 15:11:19 +0000

    Tidy up

--- game_class.rb
+++ game_class.rb
@@ -19,6 +19,8 @@ end
 
 def turn
   roll = @dice.roll
+  puts "Your turn #{@players[0].name}"
+  puts "#{@players[0].name} rolls #{roll}"
   @players[0].square += roll
   for special_square in @special_squares
     if special_square.square_number == @players[0].square
@@ -29,7 +31,6 @@ def turn
   end
   puts "#{@players[0].name} is now on square #{@players[0].square}"
   @players.rotate!
-  puts "Your turn #{@players[0].name}"
   input = gets.chomp
   puts input
 end
